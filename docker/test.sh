2f05be6bafd690e325d848c35a6e3531c881da74
Calvin McLean <calvinmclean@email.arizona.edu>
2019-03-27 09:28:17 -0700

    Add Jetsream tests to Docker test

--- docker/test.sh
+++ docker/test.sh
@@ -12,22 +12,30 @@ echo "Waiting for postgres..."
 while ! nc -z postgres 5432; do sleep 5; done
 
 psql -c "CREATE USER atmosphere_db_user WITH PASSWORD 'atmosphere_db_pass' CREATEDB;" -U postgres -h postgres
-psql -c "CREATE DATABASE atmosphere_db WITH OWNER atmosphere_db_user;" -U postgres -h postgres
 
-./travis/check_properly_generated_requirements.sh
-pip-sync requirements.txt
-sed -i 's/DATABASE_HOST = localhost/DATABASE_HOST = postgres/' variables.ini.dist
-cp ./variables.ini.dist ./variables.ini
-./configure
-python manage.py check
-python manage.py makemigrations --dry-run --check
-patch variables.ini variables_for_testing_cyverse.ini.patch
-./configure
-pip-sync dev_requirements.txt
-./travis/check_for_dead_code_with_vulture.sh
-yapf --diff -p -- $(git ls-files | grep '\.py$')
-prospector --profile prospector_profile.yaml --messages-only -- $(git ls-files | grep '\.py$')
-python manage.py test --keepdb --noinput --settings=atmosphere.settings
-python manage.py behave --keepdb --tags ~@skip-if-cyverse --settings=atmosphere.settings --format rerun --outfile rerun_failing.features
-if [ -f 'rerun_failing.features' ]; then python manage.py behave --logging-level DEBUG --capture-stderr --capture --verbosity 3 --keepdb @rerun_failing.features; fi
-python manage.py makemigrations --dry-run --check
+function run_tests_for_distribution() {
+  psql -c "CREATE DATABASE atmosphere_db WITH OWNER atmosphere_db_user;" -U postgres -h postgres
+  ./travis/check_properly_generated_requirements.sh
+  pip-sync requirements.txt
+  sed -i 's/DATABASE_HOST = localhost/DATABASE_HOST = postgres/' variables.ini.dist
+  cp ./variables.ini.dist ./variables.ini
+  ./configure
+  python manage.py check
+  python manage.py makemigrations --dry-run --check
+  patch variables.ini variables_for_testing_$1.ini.patch
+  ./configure
+  pip-sync dev_requirements.txt
+  ./travis/check_for_dead_code_with_vulture.sh
+  yapf --diff -p -- $(git ls-files | grep '\.py$')
+  prospector --profile prospector_profile.yaml --messages-only -- $(git ls-files | grep '\.py$')
+  python manage.py test --keepdb --noinput --settings=atmosphere.settings
+  python manage.py behave --keepdb --tags ~@skip-if-$1 --settings=atmosphere.settings --format rerun --outfile rerun_failing.features
+  if [ -f 'rerun_failing.features' ]; then python manage.py behave --logging-level DEBUG --capture-stderr --capture --verbosity 3 --keepdb @rerun_failing.features; fi
+  python manage.py makemigrations --dry-run --check
+  psql -c "DROP DATABASE atmosphere_db" -U postgres -h postgres
+}
+
+echo "----- RUNNING TESTS FOR CYVERSE -----"
+run_tests_for_distribution cyverse
+echo "----- RUNNING TESTS FOR JETSTREAM -----"
+run_tests_for_distribution jetstream
